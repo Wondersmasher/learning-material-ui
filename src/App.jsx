4bafb103a200529dd646f2e9eb461934fccdb029
wondersmasher <wonderfulakinniyi@gmai.com>
2023-04-25 13:00:06 +0100

    Set up routes, created a new theme in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateNote from "./pages/CreateNote";
+import Notes from "./pages/Notes";
+import { useState } from "react";
+import SharedLayout from "./components/SharedLayout";
 function App() {
-  return <div>App</div>;
+  const [notes, setNotes] = useState([
+    {
+      note: "lkafjlkafafjadkjfak fkajfklkakfkalkflkaf",
+      title: "trials",
+      category: "work",
+      id: 0,
+    },
+    {
+      note: "lkafjlkafafjadkjfak fkajfklkakfkalkflkaf",
+      title: "trials",
+      category: "work",
+      id: 1,
+    },
+    {
+      note: "lkafjlkafafjadkjfak fkajfklkakfkalkflkaf",
+      title: "trials",
+      category: "work",
+      id: 2,
+    },
+    {
+      note: "lkafjlkafafjadkjfak fkajfklkakfkalkflkaf",
+      title: "trials",
+      category: "work",
+      id: 3,
+    },
+    {
+      note: "lkafjlkafafjadkjfak fkajfklkakfkalkflkaf",
+      title: "trials",
+      category: "work",
+      id: 4,
+    },
+  ]);
+  console.log(notes);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Notes notes={notes} setNotes={setNotes} />} />
+          <Route
+            path="/create-note"
+            element={<CreateNote notes={notes} setNotes={setNotes} />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { purple } from "@mui/material/colors";
+const theme = createTheme({
+  typography: {
+    fontSize: 25,
+  },
+  palette: {
+    primary: purple,
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+  // </React.StrictMode>
+);
